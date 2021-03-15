4e04fc2e618dd27def5b8173587ce5d0818775e1
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-15 22:33:33 +0100

    Add files via upload

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,1 +1,11 @@
-
+// Only change code below this line
+function convertToInt(str) {
+        str = parseInt();
+    
+}
+convertToInt();
+console.log(parseInt("BA", 16));
+console.log(parseInt("F1", 16));
+console.log(parseInt("JeffBezos", 16));
+// Only change code above this line
+module.exports = convertToInt;
