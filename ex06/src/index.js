a56c4be22a8866bc6fd14daa9955ece447d14f68
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-15 22:26:57 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,14 @@
+// Only change code below this line
+function xSix(x) {
+    return x * 6;
 
+}
+xSix();
+
+console.log(xSix(5)); // Change this line
+console.log(xSix(2)); // Change this line
+console.log(xSix(0)); // Change this line
+
+// Only change code above this line
+
+module.exports = xSix;
