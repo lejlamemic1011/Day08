0feeace7f3c6a34911e14ff97d05eeb26df17e63
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-15 22:27:26 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,20 @@
+// Only change code below this line
 
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+
+}
+function addFive() {
+    sum = sum + 5;
+}
+console.log(addThree(sum));
+console.log(addFive(sum));
+addThree();
+addFive();
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
