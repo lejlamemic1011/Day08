4de77ce1055532868e568ca4ef106c97b79f6eda
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-15 22:25:47 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,18 @@
+// Only change code below this line
+function localScope() {
+    var myVariable = "I am here!";
+    console.log(myVariable);
 
+}
+localScope();
+
+// Only change code above this line
+
+// myVariable is not defiend outside of localScope
+if (typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable)
+} else {
+    console.log('outside localScope UNDEFINED!!!')
+}
+
+module.exports = localScope;
