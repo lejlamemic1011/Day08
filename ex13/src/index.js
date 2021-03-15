537394811ffd0a70aee877a436314d61e66015a7
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-15 22:32:20 +0100

    Add files via upload

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,1 +1,10 @@
-
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber) {
+minNumber = 5;
+maxNumber = 25;
+return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber
+}
+randomRangeNumber();
+console.log(randomRangeNumber());
+// Only change code above this line
+module.exports = randomRangeNumber;
